bd4f98ce695e22c2cebb21b0104f6c9eb4bbaf67
Saugat Rimal <saugatrimal60@gmail.com>
2022-12-16 11:21:03 +0545

    filmpire simple version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect");
+    document.title = `You clicked ${count} times`;
+  }, [count]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCount((preCount) => preCount - 1)}>-</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((preCount) => preCount + 1)}>+</button>
     </div>
   );
-}
+};
 
 export default App;
